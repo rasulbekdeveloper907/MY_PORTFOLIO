88998b3bd22a00198d3c755dcbab71944edcefa6
RUZMETOV RASULBEK <159600511+rasulbekdeveloper907@users.noreply.github.com>
2025-08-09 16:42:15 +0500

    Add files via upload

---
+++ AIR_QUALITY_CHECKER/main.py
@@ -0,0 +1,34 @@
+{
+    "version": "0.2.0",
+    "configurations": [
+        {
+            "name": "Python: Air Quality Prediction",
+            "type": "python",
+            "request": "launch",
+            "program": "${workspaceFolder}/main.py",
+            "console": "integratedTerminal",
+            "justMyCode": true,
+            "env": {
+                "PYTHONPATH": "${workspaceFolder}"
+            }
+        },
+        {
+            "name": "Python: Current File",
+            "type": "python",
+            "request": "launch",
+            "program": "${file}",
+            "console": "integratedTerminal"
+        },
+        {
+            "name": "Jupyter Notebook Debug",
+            "type": "python",
+            "request": "launch",
+            "program": "${file}",
+            "console": "integratedTerminal",
+            "args": [
+                "--NotebookApp.token=''",
+                "--NotebookApp.password=''"
+            ]
+        }
+    ]
+}
